bbc438b1651c3f814819e4bebc9ddca6ccf178a7
Vachupado <valentinoghitti00@hotmail.com>
2021-12-10 20:59:49 -0300

    Profile

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container-fluid">
-    <div class="row min-vh-100 flex-column flex-md-row">
-      <aside class="col-12 col-md-3 col-xl-2 p-0 bg-dark flex-shrink-1">
-        <nav class="navbar min-vh-100 navbar-expand-md navbar-dark bd-dark flex-md-column flex-row align-items-center py-2 text-center sticky-top" id="asidemenu">
+  <b-container class="w-100">
+    <b-row>
+      <aside v-if="loggedUser" class="bg-dark">
+        <nav class="navbar  navbar-expand-md navbar-dark bd-dark flex-md-column flex-row align-items-center py-2 text-center ">
           <div class="text-center p-3">
-            <img height="80px" src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" class="img-fluid rounded-circle my-4 p-1 d-md-block shadow" alt="">
+            <img src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" class="img-fluid rounded-circle my-4 p-1 d-md-block shadow" alt="">
             <a href="#" class="navbar-brand mx-0 font-weight-bold">{{ loggedUser.name }}</a>
             <br>
             <span class="text-white font-weight-bold">{{ loggedUser.role.role }}</span>
@@ -21,14 +21,16 @@
                   <a href="#" class="nav-link text-white"><span class="material-icons">person</span> Roles</a></li>
               </ul>
             </b-col>
-            <b-col>
-              <a href="#" @click="logout({ name: 'Vachu', email: 'vachucrack@gmail.com', logged: false })" class="nav-link text-white"><span class="material-icons">person</span> logout</a>
-            </b-col>
           </b-row>
         </nav>
+        <div>
+            <b-col class=" text-center">
+              <a href="#" @click="logout({ name: 'Vachu', email: 'vachucrack@gmail.com', logged: false })" class="nav-link text-white"><span class="material-icons">person</span> logout</a>
+            </b-col>
+        </div>
       </aside>
-    </div>
-  </div>
+    </b-row>
+  </b-container>
   <!--<a href="#"><span class="material-icons">person</span></a>-->
 </template>
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="home">
-    <router-link to="/login">adasdasdasd</router-link>
-    <button @click="test4">asdasdasdasdadadpene</button>
-    <table>
-      <tr>
-        <th>nombre</th>
-        <th>id</th>
-        <th>permisos</th>
-      </tr>
-      <tr v-for="item in test" :key="item.id">
-        <td>{{ item.role }}</td>
-        <td>{{ item.id }}</td>
-        <td>{{ item.permissions}}</td>
-        <button v-b-modal.modal-2>EL EXEQUIEL ES UN RETRASDAO</button>
-      </tr>
-    </table>
-    <b-modal id="modal-2" title="Modifica el rol" cancel-variant="danger" ok-variant="success" @ok="test3({role: name, id: 1, permissions: valores})">
-      <b-form>
-        <label for="txt_name" class="mb-2">Ingrese su nombre</label>
-        <b-form-input id="txt_name" class="mb-3" v-model="name" :value="name"></b-form-input>
-        <label for="txt_party" class="mb-2">elegir permisos</label>
-        <b-form-input id="txt_party" class="mb-3" v-model="valores" :value="valores"></b-form-input>
-      </b-form>
-    </b-modal>
-    <AsideMenu></AsideMenu>
+    <b-container fluid class="bg-secondary" >
+      <b-row>
+        <b-col  md="2">
+          <AsideMenu/>
+        </b-col>
+        <b-col md="7">
+          <h2>Profile</h2>
+          <div class="d-flex justify-content-between">
+            <img class="rounded-3" src="https://es.web.img3.acsta.net/pictures/19/05/22/10/42/3773139.jpg" width="130px" height="150px" alt="img">
+            <div>
+              <label for="img"> Image</label>
+              <br>
+              <input class="rounded-3" type="text">
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
